6da75a0b10031240f1bc37033049967b7de2eff0
wokevin <kevin.j.wood@gmail.com>
2026-05-21 18:54:25 -0700

    Updates

---
+++ assets/resume-data-BujlCOGe.js
@@ -0,0 +1,6 @@
+var e={education:`Bachelor of Science in Computer Science Western Washington University 2007 to 2010 
+Associates of Science in Computer Science Skagit Valley College 2007`,skills:` Programming: C#, .NET, WCF, WPF, LINQ, XAML, SQL, PowerShell 
+ Web Technologies: HTML, CSS, JavaScript, Angular.js, Backbone.js, JQuery, ASP.NET, Entity Framework, nHibernate, 
+Selenium Webdriver, XML, SOAP, REST 
+ Tools: Git, SVN, Team City, Visual Studio, ALM, SQL Server, Jenkins, Grunt, SSRS 
+ Problem-Solving, Leadership, Analytical Skills`,lastUpdated:`2026-05-22T01:54:23.025Z`};export{e as default};
