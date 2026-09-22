0c606138652bd67862649e6f034daa67bf10ffef
wokevin <kevin.j.wood@gmail.com>
2026-09-22 14:15:57 -0700

    updating email address to the correct one

--- src/config/siteConfig.js
+++ src/config/siteConfig.js
@@ -3,7 +3,7 @@ const siteConfig = {
   personal: {
     name: 'Kevin Wood',
     title: 'Software Developer',
-    email: 'kevin.wood@example.com', 
+    email: 'your-actual-email@gmail.com', 
   },
   social: {
     github: 'https://github.com/wokevin',
