9a78c4ba5e54194e21ffe583b561fd332acd70ba
wokevin <kevin.j.wood@gmail.com>
2026-09-22 13:33:12 -0700

    Updates

---
+++ assets/resume-data-DyLcL07T.js
@@ -0,0 +1,16 @@
+var e={education:`Computer Science, Western Washington University, 2007 - 2010 
+Associates of Science Computer Science, Skagit Valley College, 2007 
+Intermediate Leadership Course, USMC, 2004`,skills:`Programming
+Experienced: C#, .NET
+Familiar: C, C++, Haskell, Prolog, LISP, Ada, Java
+Web Programming:
+Experienced:
+HTML
+CSS
+JavaScript
+Angular.js
+Backbone.js
+JQuery
+ASP.NET
+XML
+Problem solving through requirements collection, algorithms analysis and solutions design`,pdfFileName:`Kevin Wood CodeForAmerica Software Engineering Manager Resume.pdf`,lastUpdated:`2026-09-22T20:33:10.938Z`};export{e as default};
